118321852c897d2be0f81155e189c5a34d250004
Yavuz Selim Çakmak <95148861+yavuzs-cakmak@users.noreply.github.com>
2026-07-10 20:50:22 +0300

    Update App.jsx

---
+++ src/pages/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from "./pages/Linked";
+
+function App() {
+  return (
+    <div className="page-wrapper">
+      <main className="content">
+        <Layout /> 
+      </main>
+    </div>
+  );
+}
+
+export default App;
