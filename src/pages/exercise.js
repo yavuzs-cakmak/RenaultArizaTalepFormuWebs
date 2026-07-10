ad225d26847efbe8dea73b0dff33c8305dbdbe03
Yavuz Selim Çakmak <95148861+yavuzs-cakmak@users.noreply.github.com>
2026-07-10 20:57:40 +0300

    Delete src/pages/exercise.js

--- src/pages/exercise.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'; 
-function Exercise(props){ 
-  const names = props.names; 
-  const ListItems = names.map((personName, index) => 
-  <li key={index}>Hello, {personName}</li>); 
-  return <ul>{ListItems}</ul>; 
-} 
-export default Exercise;
