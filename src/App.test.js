b5815b569a17cff3283b539100b58ec80c87342c
Yavuz Selim Çakmak <95148861+yavuzs-cakmak@users.noreply.github.com>
2026-07-10 20:55:25 +0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders home link in header', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByText(/Anasayfa/i);
   expect(linkElement).toBeInTheDocument();
 });
